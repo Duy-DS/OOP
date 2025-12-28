3ecaf5fa648622da4b30523fe04edf9485b78519
DY-DS <199239626+Duy-DS@users.noreply.github.com>
2025-12-28 10:07:48 +0700

    Add files via upload

---
+++ De_22_23/Book.java
@@ -0,0 +1,70 @@
+public class Book {
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty=0;
+
+    public Book(String name, Author[] authors, double price){
+        this.name=name;
+        this.authors= new Author[authors.length];
+        for(int i=0; i<authors.length; i++){
+            this.authors[i] = new Author(authors[i].getName(),
+                    authors[i].getEmail(),
+                    authors[i].getGender());
+        }
+        this.price=price;
+    }
+
+    public Book(String name, Author[] authors, double price, int qty){
+        this.name=name;
+        this.price=price;
+        this.qty=qty;
+        this.authors= new Author[authors.length];
+        for(int i=0; i<authors.length; i++){
+            this.authors[i] = new Author(authors[i].getName(),
+                    authors[i].getEmail(),
+                    authors[i].getGender());
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Author[] getAuthors(){
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString(){
+        return "name: "+name+" author: " + authors+" price: "+price+" qty: "+qty;
+    }
+
+    public String getAuthorNames(){
+        String result = "";
+        for (int i=0; i<this.authors.length-1; i++){
+            result += this.authors[i].getName();
+            result +=", ";
+        }
+        result += this.authors[this.authors.length-1].getName();
+        return  result;
+    }
+
+
+}
